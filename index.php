63b5572dd3caeff969c3caf98c3a48aca9a7592d
Tolga Akyol <tolgaakyol@live.com>
2022-08-12 18:15:06 +0300

    Minor fix to class autoloading.

--- index.php
+++ index.php
@@ -14,6 +14,7 @@ foreach (glob(DIR_HELPERS . '*.php') as $fileName)
 # System autoloader
 spl_autoload_register(function ($class)
 {
+    $class = str_replace('\\', '/', $class);
     include $class . '.php';
 });
 
