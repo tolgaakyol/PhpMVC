8e599f3a32c13c9ce0c7aa666f309af38d864336
Tolga Akyol <tolgaakyol@live.com>
2022-08-26 16:06:15 +0300

    Improved logging.

--- System/Log.php
+++ System/Log.php
@@ -6,27 +6,36 @@ use DateTime;
 
 enum LogType
 {
-    case Error;
     case Info;
-    case Notice;
+    case Error;
     case Warning;
     case Critical;
 }
 
 class Log
 {
-    public static function toFile(LogType $type, string $caller, string $message)
+    /**
+     * @param LogType $type Select one from the LogType enum. (Options are: Info, Error, Warning, Critical)
+     * @param string $caller Simply pass in the PHP's __METHOD__ magic constant. (60 chars max.)
+     * @param string $message Pass in the message that you want to log.
+     * @return bool Returns true if successful.
+     */
+    public static function toFile(LogType $type, string $caller, string $message): bool
     {        
         $date = new DateTime("now");
 
         // File name based on the log type and the date.
         $file = DIR_LOGS . $date->format('Y-m') . '.log';
 
-        // Format the string that will be passed into the log file.        
+        // Clamp the string inputs
+        $caller = substr($caller, 0, 60);
         $caller = str_pad($caller, 60, " ");
         $type = strtoupper(str_pad($type->name, 10, " "));
-        $log = $date->format('Y-m-d H:i:s') . "\t\t" . $type . " $caller " . $message . "\n"; // FIXME - Format $message in a way that the lines will include multiple aligned columns
+        $message = substr($message, 0, 255);
+        
+        // Format the string that will be passed into the log file.
+        $log = $date->format('Y-m-d H:i:s') . "\t\t" . $type . "\t\t". "$caller " . $message . "\n"; // FIXME - Format $message in a way that the lines will include multiple aligned columns
 
-        error_log($log, 3, $file);
+        if(error_log($log, 3, $file)){ return true; } else { return false; }
     }
 }
