d68bafe1f3c0c537272b81a3640835f4e38ca1cf
Tolga Akyol <tolgaakyol@live.com>
2023-06-07 10:46:35 +0300

    Allows easier installation and usage of the framework via composer. [WIP]

---
+++ Application.php
@@ -0,0 +1,98 @@
+<?php
+
+namespace TolgaAkyol\PhpMVC;
+
+use TolgaAkyol\PhpMVC\Config as Config;
+
+class Application {
+  public static bool $isFrameworkDev;
+  public static string $PATH_CORE;
+  public static string|bool $PATH_EXT;
+  public static string|false $PROJECT_PREFIX;
+
+  public function __construct(bool $mvcDev = false, bool $errorReportingOverride = false, string|bool $projectNamespace = false) {
+    ini_set('session.use_strict_mode', true);
+    session_start();
+
+    self::$isFrameworkDev = $mvcDev;
+    self::$PATH_CORE = $mvcDev ? '' : $this->findPath('tolgaakyol\php-mvc\Application.php', 'php-mvc');
+    self::$PATH_EXT = $mvcDev ? false : $this->findPath('index.php', 'src');
+	  self::$PROJECT_PREFIX = $projectNamespace . '\\';
+
+    !self::$isFrameworkDev && $this->createHtaccess();
+
+    # Load config files
+    foreach (glob(self::$PATH_CORE . 'Config/*.php') as $fileName) {
+      include $fileName;
+    }
+
+    foreach (glob(Config\DIR_HELPERS . '*.php') as $fileName) {
+      include $fileName;
+    }
+
+    if(!self::$isFrameworkDev && self::$PATH_EXT) {
+      foreach (glob( self::$PATH_EXT. 'Config/*.php') as $fileName) {
+        include $fileName;
+      }
+
+      foreach (glob(self::$PATH_EXT . 'Helpers/*.php') as $fileName) {
+        include $fileName;
+      }
+    }
+
+    # Error reporting
+    $errorReportingOverride && error_reporting(Config\ERROR_REPORTING);
+
+    # System autoloader
+    spl_autoload_register(function ($class) {
+      $class = str_replace('\\', '/', $class);
+      $class = str_replace('TolgaAkyol/PhpMVC/', '', $class);
+      include_once $class . '.php';
+    });
+
+    # Instantiate bootstrap
+    new System\Bootstrap;
+  }
+
+  private function findPath(string $fileName, $directoryToSelect): string|false {
+    $files = get_included_files();
+    $path = false;
+    foreach($files as $file) {
+      if(str_contains($file, $fileName)) {
+        $path = $file;
+        break;
+      }
+    }
+
+    if(!$path) {
+      return false;
+    } else {
+      $directories = explode('\\', $path);
+      $vendorKey = array_search($directoryToSelect, $directories);
+      $directories = array_splice($directories, 0, $vendorKey+1);
+      return implode('/', $directories) . '/';
+    }
+  }
+
+  private function createHtaccess(): void {
+    $path = self::$PATH_EXT . '.htaccess';
+
+    if(file_exists($path)) {
+      return;
+    }
+
+    $file = fopen(self::$PATH_EXT . '.htaccess', 'w');
+
+    $contents = <<<'EOD'
+    RewriteEngine On
+
+    RewriteCond %{REQUEST_FILENAME} !-f
+    RewriteCond %{REQUEST_FILENAME} !-d
+    RewriteCond %{REQUEST_FILENAME} !-l 
+    
+    RewriteRule ^(.+) index.php?url=$l [QSA,L]
+    EOD;
+
+    fwrite($file, $contents);
+  }
+}
--- System/Controller.php
+++ System/Controller.php
@@ -2,27 +2,33 @@
 
 namespace TolgaAkyol\PhpMVC\System;
 
+use TolgaAkyol\PhpMVC\Application;
 use TolgaAkyol\PhpMVC\Config as Config;
 
 class Controller
 {
-    public function model($modelName):mixed {
-        $fileName = Config\DIR_MODELS . $modelName . '.php';
-        $instance = ucwords(Config\DIR_MODELS) . $modelName;
-        $instance = str_replace('/', '\\', $instance);
-        $instance = Config\PACKAGE_PREFIX . $instance;
+  public function model($modelName, bool $isCore = false): mixed
+  {
+    $prefix = $isCore ? Application::$PATH_CORE : Application::$PATH_EXT;
+    $fileName = $prefix . Config\DIR_MODELS . $modelName . '.php';
+    $instance = ucwords(Config\DIR_MODELS) . $modelName;
+    $instance = str_replace('/', '\\', $instance);
+    $instance = Config\PACKAGE_PREFIX . $instance;
 
-        if (file_exists($fileName))
-        {
-            include $fileName;
-            return new $instance;    
-        } else {
-            exit('Model file not found'); // ERRMSG
-        }
+    if (file_exists($fileName)) {
+      include $fileName;
+      return new $instance;
+    } else {
+      exit('Model file not found'); // ERRMSG
     }
+  }
 
-    public function view($viewName, $content = null): void {
-        if (!empty($content) && is_array($content)) { extract($content); }
-        include Config\DIR_VIEWS . $viewName . '.php';
+  public function view($viewName, $content = null, bool $isCore = false): void
+  {
+    if (!empty($content) && is_array($content)) {
+      extract($content);
     }
+    $prefix = $isCore ? Application::$PATH_CORE : Application::$PATH_EXT;
+    include $prefix . Config\DIR_VIEWS . $viewName . '.php';
+  }
 }
