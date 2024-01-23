9d7d4d4b01d42a85b760b6414035673a7890f322
Tolga Akyol <tolgaakyol@live.com>
2024-01-23 22:10:17 +0300

    Error handling [WIP]

--- Helpers/ErrorType.php
+++ Helpers/ErrorType.php
@@ -2,13 +2,13 @@
 
 namespace TolgaAkyol\PhpMVC\Helpers;
 
-enum ErrorType {
-  case UserWrongCredentials;
-  case UserFailedRecaptcha;
-  case UserEmailExists;
-  case UserNameExists;
-  case UserEmailNotFound;
-  case UserInvalidToken;
-  case UserEmptyToken;
-  case UserExpiredToken;
+enum ErrorType: int {
+  case UserWrongCredentials = 1001;
+  case UserFailedRecaptcha = 1002;
+  case UserEmailExists = 1003;
+  case UserNameExists = 1004;
+  case UserEmailNotFound = 1005;
+  case UserInvalidToken = 1006;
+  case UserEmptyToken = 1007;
+  case UserExpiredToken = 1008;
 }
